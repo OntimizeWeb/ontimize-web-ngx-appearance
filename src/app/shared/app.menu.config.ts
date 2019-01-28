806e9e00bd3ab50e86655d54df74b97b07a63331
Patricia Martinez <patricia.martinez@imatia.com>
2019-01-28 16:17:00 +0100

    Update menu

--- src/app/shared/app.menu.config.ts
+++ src/app/shared/app.menu.config.ts
@@ -2,12 +2,10 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  {
-    id: 'appearance', name: 'APPERANCE', icon: 'web', opened: true,
-    items: [
-      { id: 'appearance-standard', name: 'STANDARD', route: '/main/appearance/standard',icon: 'input' },
-      { id: 'appearance-fill', name: 'FILL', route: '/main/appearance/fill',icon:'featured_video' },
-      { id: 'appearance-outline', name: 'OUTLINE', route: '/main/appearance/outline',icon: 'tab' },
-    ]
-  }
+
+  { id: 'appearance-standard', name: 'STANDARD', route: '/main/appearance/standard', icon: 'input' },
+  { id: 'appearance-fill', name: 'FILL', route: '/main/appearance/fill', icon: 'featured_video' },
+  { id: 'appearance-outline', name: 'OUTLINE', route: '/main/appearance/outline', icon: 'tab' },
+
+
 ];
