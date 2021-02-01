647c0673e1b3c340043a0dfff365d8dab1abe675
Paco Gomez Imatia <francisco.gomez@imatia.com>
2021-02-01 11:50:27 +0100

    Appearance outline not working fine, will look better on 8.1.2 release

--- src/app/main/extra-outside/extra-outline.module.ts
+++ src/app/main/extra-outside/extra-outline.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
+
 import { SharedModule } from '../../shared/shared.module';
 import { ExtraOutlineRoutingModule } from './extra-outline-routing.module';
 import { ExtraOutlineComponent } from './extra-outline.component';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   imports: [
