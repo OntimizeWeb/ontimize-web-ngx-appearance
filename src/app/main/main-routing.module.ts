a2d74d830fe4dc0fb2e6aa31bfd06b6ba0a2198f
Paco Gomez Imatia <francisco.gomez@imatia.com>
2021-02-03 16:31:47 +0100

    Fix main routing module to make fill page work

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MainComponent } from './main.component';
-import { HomeModule } from './home/home.module';
-import { ExtraStandardModule } from './extra-standard/extra-standard.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { ExtraFillModule } from './extra-fill/extra-fill.module';
 import { ExtraOutlineModule } from './extra-outside/extra-outline.module';
+import { ExtraStandardModule } from './extra-standard/extra-standard.module';
+import { HomeModule } from './home/home.module';
+import { MainComponent } from './main.component';
 
 export function loadHomeModule() {
   return HomeModule;
@@ -30,7 +31,7 @@ export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
       { path: 'standard', loadChildren: () => import('./extra-standard/extra-standard.module').then(m => m.ExtraStandardModule) },
-      { path: 'fill', loadChildren: () => import('./extra-standard/extra-standard.module').then(m => m.ExtraStandardModule) },
+      { path: 'fill', loadChildren: () => import('./extra-fill/extra-fill.module').then(m => m.ExtraFillModule) },
       { path: 'outline', loadChildren: () => import('./extra-outside/extra-outline.module').then(m => m.ExtraOutlineModule) },
       { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }
     ]
